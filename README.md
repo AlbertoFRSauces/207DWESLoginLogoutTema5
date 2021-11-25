974a464463e10d56380c93e239fe0864662f040d
Alberto <alberto.ferram@educa.jcyl.es>
2021-11-25 19:39:39 +0100

    25/11/2021 Creación de la estructura del Proyecto LoginLogoutTema5. Configuración del gitignore Configuración del archivo configDBPDO Configuración de README.md Configuración de indexProyectoLoginLogoutTema5.php con boton "Iniciar Sesion" y "Salir" Configuración de CSS Configuración de scripts(Crea, Carga, Borra) tanto para explotacion como para desarrollo. Configuración .htaccess Configuración de archivos de errores(400, 403, 404, 500) para el .htaccess Introducción de librería de validación Add de login.php vacio

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # 207DWESLoginLogoutTema5
 Desarrollo de una aplicación con control de acceso e identificación del usuario.
+
+## Autor: Alberto Fernandez Ramirez
+
+**Fecha Inicio Proyecto: 24/11/2021**
+
+**Ultima Actualización: 25/11/2021**
+
+## Descripción 
+Desarrollo de una aplicación con control de acceso e identificación del usuario.
+
+## Funcionalidades
+- Control de acceso, identificación de usuario 
+- Control de primer acceso y de ultimo acceso de usuarios
+- Control de acceso a funcionalidades mediante perfiles de usuario
+
+## Front end
+- HTML
+- CSS
+
+## Back end
+- PHP 7.4
+- MySQL 8.0.26
+
+## Entornos
+### Desarrollo (local)
+- http://daw207.sauces.local/207DWESLoginLogoutTema5/login.php
+### Explotación (1&1)
+- http://daw207.ieslossauces.es/207DWESLoginLogoutTema5/login.php
---
+++ codigoPHP/login.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * @author: Alberto Fernandez Ramirez
+ * @since: 25/11/2021
+ * @version: 1.0 Realizacion del ejercicio
+ * @copyright: Copyright (c) 2021, Alberto Fernandez Ramirez
+ * Desarrollo de una aplicación (ProyectoLoginLogoff)con control de acceso e identificación del usuario basado en un formulario (Login.php) con un botón de "Entrar" 
+ * y en el uso de una tabla "Usuario" de la base de datos (PDO)
+ */
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Alberto Fernandez Ramirez">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="index, follow">
+        <meta name="application-name" content="Login y logout">
+        <meta name="description" content="Control de acceso e identificación de un usuario">
+        <link href="../webroot/css/estiloejercicio.css" rel="stylesheet" type="text/css">
+        <link rel="icon" href="../webroot/css/img/home.png" type="image/x-icon">
+        <title>Login Tema 5</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
+
